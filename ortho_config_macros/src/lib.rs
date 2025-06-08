4f2f724e5a3a99e1558e1f58a7f5e25120e6b9f1
Leynos <leynos@troubledskies.net>
2025-06-08 21:47:23 +0100

    Expose generated CLI structs (#5)
    * feat(macros): expose generated CLI struct
    
    * Expose CLI structs via re-export

--- ortho_config_macros/src/lib.rs
+++ ortho_config_macros/src/lib.rs
@@ -52,6 +52,7 @@ pub fn derive_ortho_config(input: TokenStream) -> TokenStream {
 
     let cli_ident = format_ident!("__{}Cli", ident);
     let cli_mod = format_ident!("__{}CliMod", ident);
+    let cli_pub_ident = format_ident!("{}Cli", ident);
 
     let cli_fields = fields.iter().map(|f| {
         let name = f.ident.as_ref().expect("named field");
@@ -74,17 +75,19 @@ pub fn derive_ortho_config(input: TokenStream) -> TokenStream {
             use std::option::Option as Option;
             #[derive(clap::Parser, serde::Serialize)]
             #[command(rename_all = "kebab-case")]
-            pub(super) struct #cli_ident {
+            pub struct #cli_ident {
                 #( #cli_fields, )*
             }
         }
 
+        pub use #cli_mod::#cli_ident as #cli_pub_ident;
+
         impl #ident {
             #[allow(dead_code)]
-                S: Into<std::ffi::OsString> + Clone,
-                Self::load_from_iter(std::env::args_os())
+            pub fn load_from_iter<I>(args: I) -> Result<Self, ortho_config::OrthoError>
+            where
                 I: IntoIterator,
-                I::Item: AsRef<::std::ffi::OsStr>,
+                I::Item: Into<std::ffi::OsString> + Clone,
             {
                 use clap::Parser as _;
                 use figment::{Figment, providers::{Toml, Env, Serialized, Format}, Profile};
