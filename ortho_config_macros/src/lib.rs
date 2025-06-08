1cd222ae59c5c98ea150e7f7c7c6312ea2d3e4a2
Leynos <leynos@troubledskies.net>
2025-06-08 23:24:49 +0100

    feat(macros): implement attribute handling (#9)

--- ortho_config_macros/src/lib.rs
+++ ortho_config_macros/src/lib.rs
@@ -8,7 +8,74 @@
 
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::{Data, DeriveInput, Fields, GenericArgument, PathArguments, Type, parse_macro_input};
+use syn::{
+    Attribute, Data, DeriveInput, Expr, Fields, GenericArgument, Lit, PathArguments, Type,
+    parse_macro_input,
+};
+
+#[derive(Default)]
+struct StructAttrs {
+    prefix: Option<String>,
+}
+
+#[derive(Default)]
+struct FieldAttrs {
+    cli_long: Option<String>,
+    cli_short: Option<char>,
+    default: Option<Expr>,
+}
+
+fn parse_struct_attrs(attrs: &[Attribute]) -> Result<StructAttrs, syn::Error> {
+    let mut out = StructAttrs::default();
+    for attr in attrs {
+        if !attr.path().is_ident("ortho_config") {
+            continue;
+        }
+        attr.parse_nested_meta(|meta| {
+            if meta.path.is_ident("prefix") {
+                let val = meta.value()?.parse::<Lit>()?;
+                if let Lit::Str(s) = val {
+                    out.prefix = Some(s.value());
+                } else {
+                    return Err(syn::Error::new(val.span(), "prefix must be a string"));
+                }
+            }
+            Ok(())
+        })?;
+    }
+    Ok(out)
+}
+
+fn parse_field_attrs(attrs: &[Attribute]) -> Result<FieldAttrs, syn::Error> {
+    let mut out = FieldAttrs::default();
+    for attr in attrs {
+        if !attr.path().is_ident("ortho_config") {
+            continue;
+        }
+        attr.parse_nested_meta(|meta| {
+            if meta.path.is_ident("cli_long") {
+                let val = meta.value()?.parse::<Lit>()?;
+                if let Lit::Str(s) = val {
+                    out.cli_long = Some(s.value());
+                } else {
+                    return Err(syn::Error::new(val.span(), "cli_long must be a string"));
+                }
+            } else if meta.path.is_ident("cli_short") {
+                let val = meta.value()?.parse::<Lit>()?;
+                if let Lit::Char(c) = val {
+                    out.cli_short = Some(c.value());
+                } else {
+                    return Err(syn::Error::new(val.span(), "cli_short must be a char"));
+                }
+            } else if meta.path.is_ident("default") {
+                let expr = meta.value()?.parse::<Expr>()?;
+                out.default = Some(expr);
+            }
+            Ok(())
+        })?;
+    }
+    Ok(out)
+}
 
 fn option_inner(ty: &Type) -> Option<&Type> {
     if let Type::Path(p) = ty {
@@ -26,10 +93,14 @@ fn option_inner(ty: &Type) -> Option<&Type> {
 }
 
 /// Derive macro for [`ortho_config::OrthoConfig`].
-#[proc_macro_derive(OrthoConfig)]
+#[proc_macro_derive(OrthoConfig, attributes(ortho_config))]
 pub fn derive_ortho_config(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let ident = input.ident;
+    let struct_attrs = match parse_struct_attrs(&input.attrs) {
+        Ok(a) => a,
+        Err(e) => return e.to_compile_error().into(),
+    };
 
     let fields = match input.data {
         Data::Struct(data) => match data.fields {
@@ -54,7 +125,15 @@ pub fn derive_ortho_config(input: TokenStream) -> TokenStream {
     let cli_mod = format_ident!("__{}CliMod", ident);
     let cli_pub_ident = format_ident!("{}Cli", ident);
 
-    let cli_fields = fields.iter().map(|f| {
+    let mut field_attrs = Vec::new();
+    for f in &fields {
+        match parse_field_attrs(&f.attrs) {
+            Ok(a) => field_attrs.push(a),
+            Err(e) => return e.to_compile_error().into(),
+        }
+    }
+
+    let cli_fields = fields.iter().zip(field_attrs.iter()).map(|(f, attr)| {
         let name = f.ident.as_ref().expect("named field");
         let ty = &f.ty;
         let inner = option_inner(ty);
@@ -63,12 +142,53 @@ pub fn derive_ortho_config(input: TokenStream) -> TokenStream {
         } else {
             quote! { Option<#ty> }
         };
+
+        let mut arg_tokens = quote! { long };
+        if let Some(ref long) = attr.cli_long {
+            arg_tokens = quote! { long = #long };
+        }
+        if let Some(ch) = attr.cli_short {
+            let short_token = quote! { short = #ch };
+            arg_tokens = quote! { #arg_tokens, #short_token };
+        }
+
         quote! {
-            #[arg(long, required = false)]
+            #[arg(#arg_tokens, required = false)]
             #[serde(skip_serializing_if = "Option::is_none")]
-            pub(super) #name: #cli_ty
+            pub #name: #cli_ty
         }
     });
+
+    let defaults_ident = format_ident!("__{}Defaults", ident);
+    let default_struct_fields = fields.iter().map(|f| {
+        let name = f.ident.as_ref().expect("named field");
+        let ty = &f.ty;
+        let inner = option_inner(ty);
+        let default_ty = if let Some(inner) = inner {
+            quote! { Option<#inner> }
+        } else {
+            quote! { Option<#ty> }
+        };
+        quote! {
+            #[serde(skip_serializing_if = "Option::is_none")]
+            pub #name: #default_ty
+        }
+    });
+
+    let default_struct_init = fields.iter().zip(field_attrs.iter()).map(|(f, attr)| {
+        let name = f.ident.as_ref().expect("named field");
+        if let Some(expr) = &attr.default {
+            quote! { #name: Some(#expr) }
+        } else {
+            quote! { #name: None }
+        }
+    });
+
+    let env_provider = if let Some(prefix) = &struct_attrs.prefix {
+        quote! { Env::prefixed(#prefix) }
+    } else {
+        quote! { Env::raw() }
+    };
 
     let expanded = quote! {
         mod #cli_mod {
@@ -78,6 +198,11 @@ pub fn derive_ortho_config(input: TokenStream) -> TokenStream {
             pub struct #cli_ident {
                 #( #cli_fields, )*
             }
+        }
+
+        #[derive(serde::Serialize)]
+        struct #defaults_ident {
+            #( #default_struct_fields, )*
         }
 
         pub use #cli_mod::#cli_ident as #cli_pub_ident;
@@ -102,14 +227,24 @@ pub fn derive_ortho_config(input: TokenStream) -> TokenStream {
                     .unwrap_or_else(|_| "config.toml".to_string());
 
                 let mut fig = Figment::new();
+                let defaults = #defaults_ident {
+                    #( #default_struct_init, )*
+                };
+
+                fig = fig.merge(Serialized::defaults(defaults));
+
                 if std::path::Path::new(&cfg_path).is_file() {
                     fig = fig.merge(Toml::file(&cfg_path));
                 }
 
-                fig
-                    .merge(Env::raw()
+                let env_provider = {
+                    #env_provider
                         .map(|k| Uncased::new(k.as_str().to_ascii_uppercase()))
-                        .split("__"))
+                        .split("__")
+                };
+
+                fig
+                    .merge(env_provider)
                     .merge(Serialized::from(cli, Profile::Default))
                     .extract()
                     .map_err(ortho_config::OrthoError::Gathering)
