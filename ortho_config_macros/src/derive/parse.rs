f5155aa6b31d97e53fc4741484ae762f74d9b144
Leynos <leynos@troubledskies.net>
2025-07-27 22:52:20 +0100

    Format imports in parse module

--- ortho_config_macros/src/derive/parse.rs
+++ ortho_config_macros/src/derive/parse.rs
@@ -1,8 +1,8 @@
 //! Parsing utilities for the `OrthoConfig` derive macro.
 
+use syn::parenthesized;
 use syn::{
     Attribute, Data, DeriveInput, Expr, Fields, GenericArgument, Lit, PathArguments, Token, Type,
-    parenthesized,
 };
 
 #[derive(Default)]
